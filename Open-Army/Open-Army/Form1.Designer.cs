c3e0eeb424c5065f064cc2b82eccfb94554b36ed
Mosesvb <vbmo2007@gmail.com>
2016-04-08 12:43:29 +0000

    Changed the Back color of the main form

--- Open-Army/Open-Army/Form1.Designer.cs
+++ Open-Army/Open-Army/Form1.Designer.cs
@@ -45,6 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
             this.ClientSize = new System.Drawing.Size(703, 420);
             this.Controls.Add(this.label1);
             this.Name = "frmMain";
